80062569e20f5c1d1a6c25c6f8468eed63899735
benBlueKeller <benblue22@gmail.com>
2017-09-01 17:12:54 -0700

    refactored into util and credentials

--- app.py
+++ app.py
@@ -1,21 +1,17 @@
 
 from __future__ import print_function
 import httplib2
-import os
 
 from apiclient import discovery
-from oauth2client import client
-from oauth2client import tools
-from oauth2client.file import Storage
 # begin own imports seperate
 import sys
 
-from datetime import datetime
-from datetime import timedelta
+
 import pytz
 
-
-import pdb; pdb.set_trace();
+from credentials import get_credentials
+from util import scooper_match
+from util import par_sheet_dict
 
 
 #oauth argparser
@@ -27,137 +23,9 @@ import pdb; pdb.set_trace();
 flags = None
 
 
- #first commit using atom
-
-# If modifying these scopes, delete your previously saved credentials
-# at ~/.credentials/sheets.googleapis.com-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly https://www.googleapis.com/auth/calendar'
-CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'Google Sheets API Python Quickstart'
-
-
-def get_credentials():
-    """Gets valid user credentials from storage.
-
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
-
-    Returns:
-        Credentials, the obtained credential.
-    """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'sheets.googleapis.com-python-quickstart.json')
-
-    store = Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run_flow(flow, store)
-        print('Storing credentials to ' + credential_path)
-    return credentials
-
-from util import scooper_match
-
-
 
 def main():
 
-
-    def par_sheet(values):
-        shifts = []
-        this_week = False
-        for irow, row in enumerate(values):
-            # look for the first row that has saturday in column A
-            if len(row) > 0:
-                if row[0].rstrip().lstrip().upper() == 'SATURDAY':
-                    weekdays = []
-                    for col in row:
-                        trimmed = col.rstrip().lstrip()
-                        #checks for weekdays by finding alpha strings
-                        if trimmed.isalpha() == True:
-                            weekday = trimmed
-                        elif trimmed != '' and '/' in trimmed:
-                            #split parts apart to add leading zeros to month\day
-                            spl = trimmed.split("/", 2)
-                            if len(spl[0]) != 2:
-                                spl[0] =  '0' + spl[0]
-                            if len(spl[1]) != 2:
-                                spl[1] = '0' + spl[1]
-                            date = datetime.strptime("/".join(spl), "%m/%d/%Y")
-                            weekdays.append((date, weekday))
-                    this_week = weekdays
-
-                elif this_week:
-                    counter = 0
-                    shift = []
-                    row_shifts = []
-
-                    for col in row:
-                        shift.append(col)
-                        counter += 1
-                        if counter >= 3:
-                            row_shifts.append(shift)
-                            counter = 0
-                            shift = []
-
-                    for ishift, shift in enumerate(row_shifts):
-                        if ishift < len(this_week) and "-" in shift[2]:
-                            date, weekday = this_week[ishift]
-                            time_str = shift[2].strip()
-                            if " " in time_str and not "@" in time_str:
-                                time_str, duty = time_str.split(" ", 1)
-                            # BUG: ValueError; for format "@ # #:##-{}" start_time will be wrong
-                            try:
-                                start_str, end_str = time_str.split('-', 1)
-                                start_hours, start_minutes = start_str.split(':', 1)
-                                start_hours = int(start_hours)
-                                start_minutes = int(start_minutes)
-                                if start_hours < 9: # assuming nine is the ealiest and 8 the latest start
-                                    start_hours += 12
-                                start_time = date + timedelta(hours=start_hours, minutes=start_minutes)
-                            except ValueError:
-                                print('WARNING: ValueError while finding shift start time from ' + time_str)
-                            try:
-                                end_hours, end_minutes = end_str.split(':', 1)
-                                end_hours = int(end_hours) + 12 # assuming all shifts end in pm
-                                end_minutes = int(end_minutes)
-                                end_time = date + timedelta(hours=end_hours, minutes=end_minutes)
-                            except ValueError: # for line and close shifts parse from length
-                                try:
-                                    hours_from_start = float(shift[1])
-                                    if hours_from_start >= 6:
-                                        hours_from_start += 0.5 # length does not include breaks
-                                    end_time = start_time + timedelta(hours=hours_from_start)
-                                except ValueError:
-                                    end_time = start_time + timedelta(hours=1)
-                            shifts.append(dict(name = shift[0],
-                                length=shift[1],
-                                start= start_time,
-                                end = end_time,
-                                time_str = time_str,
-                                row = irow))
-        return shifts
-
-    def par_sheet_dict(values):
-        shifts = par_sheet(values)
-        scoopers = set()
-        time_min = shifts[0]["start"]
-        time_max = shifts[0]["end"]
-        for shift in shifts:
-            scoopers = scoopers.union([shift["name"].upper()])
-            if shift["start"] < time_min:
-                time_min = shift["start"]
-            if shift["end"] > time_max:
-                time_max = shift["end"]
-        return dict(shifts=shifts, scoopers=scoopers, time_max=time_max, time_min=time_min)
 
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
@@ -225,7 +93,6 @@ def main():
 
 
             print("scooper: " + this_scooper)
-            import pdb; pdb.set_trace()
             for shift in shifts:
                 if this_scooper.upper() in shift["name"].upper():
                     #create an id that will identical, but constant, for each shift to avoid repeat events
--- util.py
+++ util.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from datetime import timedelta
+
 def scooper_match(query_scooper, possible_match):
     query_scooper = query_scooper.upper()
     split_scooper = possible_match.upper().split(' ')
@@ -9,3 +12,92 @@ def scooper_match(query_scooper, possible_match):
         if check == query_scooper:
             return True
     return False
+
+
+def par_sheet(values):
+    shifts = []
+    this_week = False
+    for irow, row in enumerate(values):
+        # look for the first row that has saturday in column A
+        if len(row) > 0:
+            if row[0].rstrip().lstrip().upper() == 'SATURDAY':
+                weekdays = []
+                for col in row:
+                    trimmed = col.rstrip().lstrip()
+                    #checks for weekdays by finding alpha strings
+                    if trimmed.isalpha() == True:
+                        weekday = trimmed
+                    elif trimmed != '' and '/' in trimmed:
+                        #split parts apart to add leading zeros to month\day
+                        spl = trimmed.split("/", 2)
+                        if len(spl[0]) != 2:
+                            spl[0] =  '0' + spl[0]
+                        if len(spl[1]) != 2:
+                            spl[1] = '0' + spl[1]
+                        date = datetime.strptime("/".join(spl), "%m/%d/%Y")
+                        weekdays.append((date, weekday))
+                this_week = weekdays
+
+            elif this_week:
+                counter = 0
+                shift = []
+                row_shifts = []
+
+                for col in row:
+                    shift.append(col)
+                    counter += 1
+                    if counter >= 3:
+                        row_shifts.append(shift)
+                        counter = 0
+                        shift = []
+
+                for ishift, shift in enumerate(row_shifts):
+                    if ishift < len(this_week) and "-" in shift[2]:
+                        date, weekday = this_week[ishift]
+                        time_str = shift[2].strip()
+                        if " " in time_str and not "@" in time_str:
+                            time_str, duty = time_str.split(" ", 1)
+                        # BUG: ValueError; for format "@ # #:##-{}" start_time will be wrong
+                        try:
+                            start_str, end_str = time_str.split('-', 1)
+                            start_hours, start_minutes = start_str.split(':', 1)
+                            start_hours = int(start_hours)
+                            start_minutes = int(start_minutes)
+                            if start_hours < 9: # assuming nine is the ealiest and 8 the latest start
+                                start_hours += 12
+                            start_time = date + timedelta(hours=start_hours, minutes=start_minutes)
+                        except ValueError:
+                            print('WARNING: ValueError while finding shift start time from ' + time_str)
+                        try:
+                            end_hours, end_minutes = end_str.split(':', 1)
+                            end_hours = int(end_hours) + 12 # assuming all shifts end in pm
+                            end_minutes = int(end_minutes)
+                            end_time = date + timedelta(hours=end_hours, minutes=end_minutes)
+                        except ValueError: # for line and close shifts parse from length
+                            try:
+                                hours_from_start = float(shift[1])
+                                if hours_from_start >= 6:
+                                    hours_from_start += 0.5 # length does not include breaks
+                                end_time = start_time + timedelta(hours=hours_from_start)
+                            except ValueError:
+                                end_time = start_time + timedelta(hours=1)
+                        shifts.append(dict(name = shift[0],
+                            length=shift[1],
+                            start= start_time,
+                            end = end_time,
+                            time_str = time_str,
+                            row = irow))
+    return shifts
+
+def par_sheet_dict(values):
+    shifts = par_sheet(values)
+    scoopers = set()
+    time_min = shifts[0]["start"]
+    time_max = shifts[0]["end"]
+    for shift in shifts:
+        scoopers = scoopers.union([shift["name"].upper()])
+        if shift["start"] < time_min:
+            time_min = shift["start"]
+        if shift["end"] > time_max:
+            time_max = shift["end"]
+    return dict(shifts=shifts, scoopers=scoopers, time_max=time_max, time_min=time_min)
