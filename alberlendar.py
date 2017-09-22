3818b0f5f6d1504bcb5353643c25691794c11fe2
benBlueKeller <benblue22@gmail.com>
2017-09-22 16:23:44 -0700

    begin writing modular functions in albertlendat.py

--- alberlendar.py
+++ alberlendar.py
@@ -1,3 +1,10 @@
+from apiclient import discovery
+discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+                'version=v4')
+
 class Alberlendar():
     """takes google credentials to parse a google spreadsheets schedule into the calendar"""
-    def __init__():
+    def __init__(*ars, **kwargs):
+
+        def get_sheets_values(scheduleID='1xJcLh9yWGmqu_Blnp4yykb4cSnnus5fU4YoHFqGEf-o', service=discovery.build('calendar', 'v3', discoveryUrl)):
+            """returns values from desired sheets"""
