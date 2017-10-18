ab5d6f75493499744413fa7baf56f90bfc34c8d7
benBlueKeller <benblue22@gmail.com>
2017-10-17 20:21:46 -0700

    refined constants and realized a seperate class needed for handling sheet

--- alberlendar.py
+++ alberlendar.py
@@ -4,15 +4,19 @@ from apiclient import discovery
 
 from credentials import get_credentials
 
-SHEETS = discovery.build('sheets', 'v4', http=http,
+CREDENTIALS = get_credentials()
+HTTP = CREDENTIALS.authorize(httplib2)
+SHEETS = discovery.build('sheets', 'v4', http=HTTP,
                          discoveryServiceUrl='https://sheets.googleapis.com/$discovery/rest?'
                          'version=v4')
+CALENDAR = discovery.build('calendar', 'v3', http=HTTP)
 
 
 class Alberlendar():
     """takes google credentials to parse a google spreadsheets schedule into the calendar"""
     def __init__(self,
-                 credentials=get_credentials(),
                  schedule_id="1xJcLh9yWGmqu_Blnp4yykb4cSnnus5fU4YoHFqGEf-o",
                  sheets=SHEETS):
+        # only thing needed from SHEETS is parsed values, so
+        # a seperate class should be need for fetching those valuse
         pass
