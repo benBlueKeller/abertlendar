3ff99c8c8788af0832813fc017a02fcee397d114
benBlueKeller <benblue22@gmail.com>
2017-10-20 15:44:09 -0700

    removed all defaults from Schedule, should be set higher for consistency

--- schedule.py
+++ schedule.py
@@ -1,22 +1,12 @@
 from datetime import datetime
 
-from apiclient import discovery
-import httplib2
-
-from credentials import get_credentials
 from util import par_sheet
 
-CREDENTIALS = get_credentials()
-# BUG: Attribute error: module has no Attribute 'request'
-HTTP = CREDENTIALS.authorize(httplib2)
-SHEETS = discovery.build('sheets', 'v4', http=HTTP,
-                         discoveryServiceUrl='https://sheets.googleapis.com/$discovery/rest?'
-                         'version=v4')
+
 class Schedule(list):
-    """Schedule turns tabs with 'CURRENT' and 'NEXT' google sheets
-        salt & straw schedule into a list of shifts"""
-    def __init__(self, schedule_id='1xJcLh9yWGmqu_Blnp4yykb4cSnnus5fU4YoHFqGEf-o',
-                 sheets=SHEETS):
+    """Schedule takes a sheets apiclient and schedule_id,
+    then turns tabs with 'CURRENT' and 'NEXT' into a list of shifts"""
+    def __init__(self, sheets, schedule_id):
         super(Schedule, self).__init__()
         spreadsheet = sheets.spreadsheets().get(spreadsheetId=schedule_id).execute()['sheets']
         self.scoopers = set()
