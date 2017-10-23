bb45f20a63097268d175f10d217ddad6c789e07e
benBlueKeller <benblue22@gmail.com>
2017-10-22 20:56:15 -0700

    Alberlendar successfully gathers shifts; added *.pyc to .gitignore

--- alberlendar.py
+++ alberlendar.py
@@ -1,22 +1,22 @@
+from apiclient import discovery
 import httplib2
 
-from apiclient import discovery
-
 from credentials import get_credentials
-
-CREDENTIALS = get_credentials()
-HTTP = CREDENTIALS.authorize(httplib2)
-SHEETS = discovery.build('sheets', 'v4', http=HTTP,
-                         discoveryServiceUrl='https://sheets.googleapis.com/$discovery/rest?'
-                         'version=v4')
-CALENDAR = discovery.build('calendar', 'v3', http=HTTP)
+from schedule import Schedule
 
 
 class Alberlendar():
     """takes google credentials to parse a google spreadsheets schedule into the calendar"""
     def __init__(self,
-                 schedule_id="1xJcLh9yWGmqu_Blnp4yykb4cSnnus5fU4YoHFqGEf-o",
-                 sheets=SHEETS):
-        # only thing needed from SHEETS is parsed values, so
-        # a seperate class should be need for fetching those valuse
-        pass
+                 schedule_id="1xJcLh9yWGmqu_Blnp4yykb4cSnnus5fU4YoHFqGEf-o"):
+        credentials = get_credentials()
+        http = credentials.authorize(httplib2.Http())
+        sheets = discovery.build('sheets', 'v4', http=http,
+                                 discoveryServiceUrl='https://sheets.googleapis.com/$discovery/rest?'
+                                 'version=v4')
+        calendar = discovery.build('calendar', 'v3', http=http)
+        shifts = Schedule(sheets=sheets, schedule_id=schedule_id)
+        import pdb; pdb.set_trace()
+
+if __name__ == '__main__':
+    alb = Alberlendar()
