599c51a76982f6e68bfaac7f7117bbb5d9a9807c
benBlueKeller <benblue22@gmail.com>
2017-10-25 22:32:12 -0700

    added argpare

--- con.py
+++ con.py
@@ -1,1 +1,11 @@
 """con is a console application for using Alberlendar"""
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='Process name of scoper')
+    parser.add_argument('--scooper', '-s')
+    args = parser.parse_args()
+    import pdb; pdb.set_trace()
+
+if __name__ == '__main__':
+    main()
