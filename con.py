7dca3731c35e0ebc1a9b400b8151accf55e14de4
benBlueKeller <benblue22@gmail.com>
2017-10-25 22:33:31 -0700

    cpmmit on proper branch

---
+++ con.py
@@ -0,0 +1,12 @@
+"""con is a console application for using Alberlendar"""
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='Process name of scoper')
+    parser.add_argument('--scooper', '-s')
+    args = parser.parse_args()
+    
+    import pdb; pdb.set_trace()
+
+if __name__ == '__main__':
+    main()
