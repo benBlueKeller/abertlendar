28a8b8052f740acd653ad01e0981e1789a89a318
benBlueKeller <benblue22@gmail.com>
2017-10-26 21:26:05 -0700

    add handling for all nescesary arguments given

--- con.py
+++ con.py
@@ -1,11 +1,17 @@
 """con is a console application for using Alberlendar"""
 import argparse
+from alberlendar import Alberlendar
 
 def main():
     parser = argparse.ArgumentParser(description='Process name of scoper')
-    parser.add_argument('--scooper', '-s')
+    parser.add_argument('scooper')
+    parser.add_argument('--cal_id', '-c')
+    parser.add_argument('--primary', '-p', action='store_true')
     args = parser.parse_args()
-    
+    if args.scooper and args.cal_id:
+        return Alberlendar(args.scooper, args.cal_id)
+
+
     import pdb; pdb.set_trace()
 
 if __name__ == '__main__':
